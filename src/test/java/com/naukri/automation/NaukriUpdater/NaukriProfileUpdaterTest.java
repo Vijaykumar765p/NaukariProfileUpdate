929a981a526df5cf9dff84d2128aba205c59eb19
Vijaykumar765p <vpattar6@gmail.com>
2025-02-19 08:09:44 +0530

    testng class data provider used

--- src/test/java/com/naukri/automation/NaukriUpdater/NaukriProfileUpdaterTest.java
+++ src/test/java/com/naukri/automation/NaukriUpdater/NaukriProfileUpdaterTest.java
@@ -4,53 +4,77 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 public class NaukriProfileUpdaterTest {
-    public static void main(String[] args) {
-        // Setup WebDriver
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito"); // Run in incognito mode (no cookies stored)
-        options.addArguments("--disable-blink-features=AutomationControlled"); // Prevent detection
-        //options.addArguments("--headless"); // Uncomment if you want to run in headless mode
-        WebDriver driver = new ChromeDriver(options);
+	private WebDriver driver;
+	private WebDriverWait wait;
 
-        try {
-            // Open Naukri Login Page
-            driver.get("https://www.naukri.com/nlogin/login");
-            driver.manage().window().maximize();
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	@BeforeClass
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--incognito");
+		options.addArguments("--disable-blink-features=AutomationControlled");
+		// options.addArguments("--headless"); // Uncomment if you want to run in
+		// headless mode
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+	}
 
-            // Click on Login
-            driver.findElement(By.linkText("Login")).click();
-            Thread.sleep(3000);
+	@DataProvider(name = "loginData")
+	public Object[][] loginDataProvider() {
+		return new Object[][] { { "vpattar6@gmail.com", "Anvit@2503" } };
+	}
 
-            // Enter Login Credentials
-            driver.findElement(By.id("usernameField")).sendKeys("vpattar6@gmail.com");
-            driver.findElement(By.id("passwordField")).sendKeys("Anvit@2503");
-            driver.findElement(By.xpath("//button[text()='Login']")).click();
-            Thread.sleep(5000);
+	@Test(dataProvider = "loginData")
+	public void testProfileUpdate(String username, String password) throws InterruptedException {
+		// Open Naukri Login Page
+		driver.get("https://www.naukri.com/nlogin/login");
 
-            // Navigate to Profile Page
-            driver.get("https://www.naukri.com/mnjuser/profile");
-            Thread.sleep(5000);          
-            // Upload Resume to Update Profile
-            //Vijaykumar_Resume_QAEngineer_ATS and VijaykumarP_SrQAEngineer_7YOE_java
-            String resumePath = System.getProperty("user.dir") + "/resume/Vijaykumar_Resume_QAEngineer_ATS.pdf";
-            WebElement uploadButton = driver.findElement(By.xpath("//input[@type='file']"));
-            uploadButton.sendKeys(resumePath);
-            Thread.sleep(5000); // Wait for upload to complete
+		// Click on Login
+		driver.findElement(By.linkText("Login")).click();
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("usernameField")));
 
-            System.out.println("Profile updated successfully!");
+		// Enter Login Credentials
+		driver.findElement(By.id("usernameField")).sendKeys(username);
+		driver.findElement(By.id("passwordField")).sendKeys(password);
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+		Thread.sleep(5000);
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            driver.quit();
-        }
-    }
+		// Navigate to Profile Page
+		driver.get("https://www.naukri.com/mnjuser/profile");
+		Thread.sleep(5000);
+
+		// Upload Resume to Update Profile
+		String resumePath = System.getProperty("user.dir") + "/resume/Vijaykumar_Resume_QAEngineer_ATS.pdf";
+		WebElement uploadButton = driver.findElement(By.xpath("//input[@type='file']"));
+		uploadButton.sendKeys(resumePath);
+		Thread.sleep(5000); // Wait for upload to complete
+		 // Assert that the profile was updated successfully
+//        WebElement successMessage = driver.findElement(By.xpath("//span[contains(text(),'Profile updated successfully')]"));
+//        Assert.assertTrue(successMessage.isDisplayed(), "Profile update was not successful");
+
+		System.out.println("Profile updated successfully!");
+	}
+
+	@AfterClass
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
